498021ffefeaa161c644e1f2f975ba075d8c697c
Vladimir Sumarov <summeroff@gmail.com>
2022-06-17 14:39:05 -0700

    github actions config

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -5,12 +5,14 @@ cd build
 cmake .. \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
--DCMAKE_INSTALL_PREFIX=${DISTRIBUTEDIRECTORY}/node-fontinfo
--DNODEJS_NAME=${RUNTIMENAME} \
--DNODEJS_URL=${RUNTIMEURL} \
--DNODEJS_VERSION=${RUNTIMEVERSION}
+-DNODEJS_VERSION=${ELECTRON_VERSION} \
+-DCMAKE_INSTALL_PREFIX=${DISTRIBUTE_DIRECTORY}/node-fontinfo
 
 cd ..
 
 # Build
 cmake --build build --target install --config RelWithDebInfo
+
+curl -sL https://sentry.io/get-cli/ | bash
+dsymutil $PWD/${BUILD_DIRECTORY}/RelWithDebInfo/node_fontinfo.node
+sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-client $PWD/${BUILD_DIRECTORY}/RelWithDebInfo/node_fontinfo.node.dSYM/Contents/Resources/DWARF/node_fontinfo.node
